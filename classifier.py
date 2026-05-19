280cf825034e0f32424ab86548838ea6541814ab
sabrinahaniff <xxcrystalwindxx@gmail.com>
2026-05-18 22:34:17 -0400

    add imports to classifier file

---
+++ classifier.py
@@ -0,0 +1,6 @@
+import json
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import numpy as np
