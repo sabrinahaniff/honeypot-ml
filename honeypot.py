056086761418c63a298f8591fafda7c014c70539
sabrinahaniff <xxcrystalwindxx@gmail.com>
2026-05-18 21:59:00 -0400

    complete logging

--- honeypot.py
+++ honeypot.py
@@ -11,3 +11,24 @@ logging.basicConfig(
     format='%(message)s'
 )
 
+HOST = '0.0.0.0'
+PORT = 2222  # fake SSH port
+
+class FakeSSHServer(paramiko.ServerInterface):
+    def __init__(self, client_ip):
+        self.client_ip = client_ip
+
+    def check_auth_password(self, username, password):
+        # log every login attempt
+        log_entry = {
+            "timestamp": datetime.now().isoformat(),
+            "ip": self.client_ip,
+            "username": username,
+            "password": password
+        }
+        logging.info(str(log_entry))
+        print(f"[ATTACK] {self.client_ip} tried {username}:{password}")
+        return paramiko.AUTH_FAILED  # always fail
+
+    def get_allowed_auths(self, username):
+        return 'password'
